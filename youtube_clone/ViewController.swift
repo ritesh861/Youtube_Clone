30994aeba69deec3c172fa85842b88673cb45050
Ritesh Singh <riteshsingh@Riteshs-MacBook-Air.local>
2021-12-19 13:19:52 +0530

    initial xcode project creation

---
+++ youtube_clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube_clone
+//
+//  Created by Ritesh Singh on 19/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
